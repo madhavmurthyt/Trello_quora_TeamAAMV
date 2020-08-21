a0615cc109cd9f145d53f00599b39f8137620d37
Madhav Thotapalli <mvthotapalli@Madhavs-MacBook-Pro-2.local>
2020-08-21 12:32:03 +0530

    Fixed Questions Controller

---
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/QuestionController.java
@@ -0,0 +1,33 @@
+package com.upgrad.quora.api.controller;
+
+import com.upgrad.quora.service.business.QuestionService;
+import com.upgrad.quora.api.model.QuestionDetailsResponse;
+import com.upgrad.quora.service.entity.QuestionEntity;
+import com.upgrad.quora.service.entity.UserEntity;
+import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import com.upgrad.quora.service.exception.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class QuestionController {
+
+    @Autowired
+    private QuestionService questionService;
+
+    @RequestMapping(method = RequestMethod.GET, path="question/all/{userId}",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<List> getAllQuestionsByUser( @PathVariable("userId") String userId,  @RequestHeader("authorization") String authorization)  throws AuthorizationFailedException, UserNotFoundException {
+            final List<QuestionEntity> questionEntity = questionService.getAllQuestionsByUser(userId, authorization);
+            List<QuestionDetailsResponse> questionDetailsResponse = new ArrayList<QuestionDetailsResponse>();
+            for (QuestionEntity questionEntity1: questionEntity) {
+                 questionDetailsResponse.add(new QuestionDetailsResponse().id(questionEntity1.getUuid()).content(questionEntity1.getContent()));
+            }
+        return new ResponseEntity<List>(questionDetailsResponse, HttpStatus.OK);
+        }
+}
---
+++ quora-service/src/main/java/com/upgrad/quora/service/dao/QuestionDao.java
@@ -0,0 +1,35 @@
+package com.upgrad.quora.service.dao;
+
+import com.upgrad.quora.service.entity.QuestionEntity;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class QuestionDao {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public List<QuestionEntity> getAllQuestionsByUser(long userId) {
+        return entityManager.createNamedQuery("allQuestionsByUser", QuestionEntity.class)
+                .setParameter("user_id", userId).getResultList();
+    }
+
+    public QuestionEntity deleteQuestion(QuestionEntity questionEntity) {
+        entityManager.remove(questionEntity);
+        return questionEntity;
+     }
+     public QuestionEntity getQuestionByUUID(String uuid) {
+        try {
+            return entityManager.createNamedQuery("questionByUuid", QuestionEntity.class).setParameter(
+                    "uuid",
+                    uuid).getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
+}
